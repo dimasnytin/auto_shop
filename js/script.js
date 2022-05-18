d77aac33e647374de98d13d89528d3501524f856
Dima Snytin <dimasnytin@gmail.com>
2022-05-18 15:07:48 +0300

    add new section Home - style - script

--- js/script.js
+++ js/script.js
@@ -38,3 +38,16 @@ window.onload = () => {
 
 }
 
+// скрипт для Home анимация - привязка к мышке для перемещения предметов
+document.querySelector('.home').onmousemove = (e) =>{
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+
+    let speed = elm.getAttribute('data-speed');
+
+    let x = (window.innerWidth - e.pageX * speed) / 90;
+    let y = (window.innerHeight - e.pageY * speed) / 90;
+
+    elm.style.transform = `translateX(${y}px) translateY(${x}px)`
+  });
+} 
+
