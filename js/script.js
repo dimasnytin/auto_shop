b2dfc285309ad07beb9f01a7a3f707e08fab5a15
Dima Snytin <dimasnytin@gmail.com>
2022-05-18 13:48:04 +0300

    Add project

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+// скрипт открытия модульного окна
+document.querySelector('#login-btn').onclick = () =>{
+  document.querySelector('.login-form-container').classList.toggle('active');
+}
+// скрипт закрытия модульного окна
+document.querySelector('#close-login-form').onclick = () => {
+  document.querySelector('.login-form-container').classList.remove('active');
+}
+
+window.onscroll = () => {
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  }else{
+    document.querySelector('.header').classList.remove('active');
+  }
+
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+window.onload = () => {
+
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+
+}
+
