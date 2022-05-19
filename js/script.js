9e4041b246568cd40442fa7b9360d2cd07b6e693
Dima Snytin <dimasnytin@gmail.com>
2022-05-19 12:50:49 +0300

    add section icons

--- js/script.js
+++ js/script.js
@@ -49,5 +49,13 @@ document.querySelector('.home').onmousemove = (e) =>{
 
     elm.style.transform = `translateX(${y}px) translateY(${x}px)`
   });
-} 
+}; 
+
+
+document.querySelector('.home').onmouseleave = () => {
+  document.querySelectorAll('.home-parallax').forEach(elm => {
+
+    elm.style.transform = `translateX(0px) translateY(0px)`
+  });
+}; 
 
