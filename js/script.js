adce4902839678294b077f9a6de82f201698065b
Dima Snytin <dimasnytin@gmail.com>
2022-05-20 13:54:42 +0300

    add section service, add section featured, add new slider2

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ document.querySelector('.home').onmouseleave = () => {
   });
 }; 
 
-// Swiper подключаем js
+// Swiper подключаем js слайдер 1
 var swiper = new Swiper(".vehicles-slider", {
   slidesPerView: 1,
   spaceBetween: 20,
@@ -87,3 +87,30 @@ var swiper = new Swiper(".vehicles-slider", {
   },
 });
 
+// Swiper подключаем js слайдер 2
+var swiper = new Swiper(".featured-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  grapCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1040: {
+      slidesPerView: 3,
+    },
+  },
+});
